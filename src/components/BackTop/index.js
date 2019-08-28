aaee5587a85ff5d541f8028ae176c9c7ec64608c
dongwudi <550293874@qq.com>
2019-08-28 23:28:02 +0800

    backtop

--- src/components/BackTop/index.js
+++ src/components/BackTop/index.js
@@ -1,15 +1,20 @@
 import React, { PureComponent } from "react";
-import './index.scss'
+import "./index.scss";
 
 class BackTop extends PureComponent {
   render() {
     return (
-      <span
-        className="m-back"
-      >
+      <span className="m-back" onClick={this.handleScrollTop}>
         回到顶部
       </span>
     );
+  }
+  handleScrollTop() {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth"
+    });
   }
 }
 
